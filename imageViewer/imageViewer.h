171d48e3ad8e46cd8bab89edc599b002bc972b3c
Michael Kirschner <mjk.kirschner@gmail.com>
2020-01-16 22:10:27 -0500

    Initial Commit

---
+++ imageViewer/imageViewer.h
@@ -0,0 +1,13 @@
+//
+//  imageViewer.h
+//  imageViewer
+//
+//  Created by Michael Kirschner on 1/16/20.
+//  Copyright © 2020 Michael Kirschner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface imageViewer : NSObject
+
+@end
---
+++ imageViewer/imageViewer.m
@@ -0,0 +1,13 @@
+//
+//  imageViewer.m
+//  imageViewer
+//
+//  Created by Michael Kirschner on 1/16/20.
+//  Copyright © 2020 Michael Kirschner. All rights reserved.
+//
+
+#import "imageViewer.h"
+
+@implementation imageViewer
+
+@end
